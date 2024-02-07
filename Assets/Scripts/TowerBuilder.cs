b15aa52120cb28cda71e44d717dac9d8bc4f332a
Soma Nyiro <nysoma@protonmail.com>
2024-02-07 21:58:56 +0100

    made button appear and disappear properly on clicking

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerBuilder : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class TowerBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             RaycastHit hit;
 
@@ -24,8 +25,14 @@ public class TowerBuilder : MonoBehaviour
                 Transform objectHit = hit.transform;
 
                 GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(objectHit.position);
+                GetComponent<CanvasGroup>().alpha = 1;
+                GetComponent<CanvasGroup>().interactable = true;
             }
-
+            else
+            {
+                GetComponent<CanvasGroup>().alpha = 0;
+                GetComponent<CanvasGroup>().interactable = false;
+            }
         }
     }
 }
