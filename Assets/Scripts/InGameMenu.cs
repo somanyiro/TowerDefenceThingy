6da6d90b85bb119f126664313f984337e9c7d6c3
Soma Nyiro <nysoma@protonmail.com>
2024-02-09 13:11:05 +0100

    added working time scale slider

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
     public CanvasGroup pauseMenu;
+    public TextMeshProUGUI timeScaleText;
 
     private bool gamePaused = false;
+    private float desiredTimeScale = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,16 @@ public class InGameMenu : MonoBehaviour
         pauseMenu.blocksRaycasts = gamePaused;
         pauseMenu.GetComponent<LayoutElement>().ignoreLayout = !gamePaused;
 
-        Time.timeScale = gamePaused ? 0f : 1f;
+        Time.timeScale = gamePaused ? 0f : desiredTimeScale;
+    }
+
+    public void ChangeTimeScale(float value)
+    {
+        //dividing by two so the slider can have fractions
+        desiredTimeScale = value / 2f;
+
+        timeScaleText.text = desiredTimeScale.ToString("0.0");
+        
+        if (!gamePaused) Time.timeScale = desiredTimeScale;
     }
 }
