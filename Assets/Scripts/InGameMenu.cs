762ee2a15b8630ff2d29e717882e4dcb1a8bb32e
Soma Nyiro <nysoma@protonmail.com>
2024-02-09 11:42:50 +0100

    made pause menu pause the game
    it also appears and disappears accordingly

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
+    public CanvasGroup pauseMenu;
+
+    private bool gamePaused = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,17 @@ public class InGameMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PauseGame()
+    {
+        gamePaused = !gamePaused;
+
+        pauseMenu.alpha = gamePaused ? 1f : 0f;
+        pauseMenu.interactable = gamePaused;
+        pauseMenu.blocksRaycasts = gamePaused;
+        pauseMenu.GetComponent<LayoutElement>().ignoreLayout = !gamePaused;
+
+        Time.timeScale = gamePaused ? 0f : 1f;
     }
 }
